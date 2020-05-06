d630cbdddb4652089084b23a9f989830c15e4300
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-06 14:37:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,20 +14,20 @@ const codes = [
 function init() {
   let index=0;
   document.body.addEventListener("keydown", (event) => {
-    const key = event.key 
-    
+    const key = event.key
+
     if (codes[index] == key) {
       index++
-    } 
-    
-    else {
-      index = 0 
     }
-    
+
+    else {
+      index = 0
+    }
+
     if (index == 10) {
       alert("Hurray!");
-      index = 0 
+      index = 0
     }
-  }); 
+  });
 
 }
