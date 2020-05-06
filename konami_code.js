dd59660785ae289ee0db033206e6ca5da107f0c1
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-06 13:42:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,7 @@ const codes = [
 
 function init() {
   let i=0;
-  document.body.addEventListener("keydown", function(e) => {
+  document.body.addEventListener("keydown", (event) => {
   }
   function konami(e){
        const key = e.key;
