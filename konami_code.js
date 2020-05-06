07c4e685e1d70635d6e5859e4fb18219a8a742e0
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-06 14:25:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,10 +17,11 @@ function init() {
     let i=0;
      const key = event.key;
     for(let i=0; i<codes.length; i++){
-      if(key === codes[i]){;}
+      if(key === codes[i]){
       if(i+1 === codes.length){
         alert('Hurray');
       }
+    }
       else{
         i=0;
       }
